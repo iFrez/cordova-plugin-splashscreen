8053816083b0496501b7490bfb59a036195b3251
Ilyaz <gimalov-ilyaz@ya.ru>
2019-01-05 03:39:43 +0400

    Update SplashScreenProxy.js

--- src/browser/SplashScreenProxy.js
+++ src/browser/SplashScreenProxy.js
@@ -25,8 +25,8 @@ var splashImageHeight = 200;
 var position = { x: 0, y: 0, width: splashImageWidth, height: splashImageHeight }; 
 var localSplash; // the image to display
 var localSplashImage;
-var bgColor = "#464646";
-var imageSrc = '/img/logo.png';
+var bgColor = "#68b8fd";
+var imageSrc = '/images/logo.png';
 var splashScreenDelay = 3000; // in milliseconds
 var showSplashScreen = true; // show splashcreen by default
 var cordova = require('cordova');
